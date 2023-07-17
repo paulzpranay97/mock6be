2971cc3c3414cd4797a79b019464f3ecc59357ab
PRANAY PAUL <paulpranay1997@gmail.com>
2023-07-17 13:56:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 require('dotenv').config();
-
+const cors = require("cors")
 const connection = require('./config/db')
 const {userRouter} = require('./routes/user.route')
 const {quizRouter} = require('./routes/quiz.route');
 
 const app = express();
 const port = process.env.port;
-
+app.use(cors())
 app.use(express.json())
 app.use("/user",userRouter);
 app.use("/quiz",quizRouter);
